e748f3687d6f22609e87ed55eb109ee7bd3f5bd4
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-24 22:47:33 +0300

    feat: implemented header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+const burgerButton = document.querySelector('.header__burger');
+
+const nav = document.querySelector('.header__nav');
+
+burgerButton.addEventListener('click', (event) => {
+  document.body.classList.toggle('active');
+  event.currentTarget.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
+nav.addEventListener('click', (event) => {
+  const isNav = event.target === event.currentTarget;
+  if (!isNav) {
+    document.body.classList.toggle('active');
+    event.currentTarget.classList.toggle('active');
+    burgerButton.classList.toggle('active');
+  }
+});
+
 const paralaxScreen = document.querySelector('.paralax-screen');
 
 const gztCopyText = document.querySelector('.gzt__contract');
