b902e5c8d65e02f621cba63fb1979ca181e77cb4
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-24 19:08:28 +0300

    feat: implemented gzt

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-const  paralaxScreen = document.querySelector('.paralax-screen');
+const paralaxScreen = document.querySelector('.paralax-screen');
+
+const gztCopyText = document.querySelector('.gzt__contract');
+
+gztCopyText.addEventListener('click', (event) => {
+  const text = event.currentTarget.innerText;
+  navigator.clipboard.writeText(text);
+});
