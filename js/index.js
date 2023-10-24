09bd19ec1f60f4c6d15f0e61b1db113825b5d119
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-25 00:38:06 +0300

    fix: fixed bg size

--- js/index.js
+++ js/index.js
@@ -21,40 +21,6 @@ nav.addEventListener('click', (event) => {
   }
 });
 
-const paralaxScreen = document.querySelector('.paralax-screen');
-
-let oldScrollY = window.scrollY;
-let scrollDirection = 'up';
-
-const paralaxTop = document.getElementById('paralaxTop');
-const paralaxBottom = document.getElementById('paralaxBottom');
-
-// window.addEventListener('scrollend', (event) => {
-//   if (oldScrollY < window.scrollY) {
-//     scrollDirection = 'down';
-//   } else {
-//     scrollDirection = 'up';
-//   }
-//   oldScrollY = window.scrollY;
-
-//   const paralaxBlockHeight = paralaxTop.offsetHeight;
-
-//   const isTopParalax = oldScrollY >= 0 && oldScrollY <= paralaxBlockHeight;
-//   const isBottomParalax = oldScrollY > paralaxBlockHeight && oldScrollY <= paralaxBlockHeight * 2;
-
-//   if (isTopParalax && scrollDirection === 'down') {
-//     console.log('top down');
-//     paralaxBottom.scrollIntoView({behavior: "smooth"})
-//   } else if (isBottomParalax && scrollDirection === 'up') {
-//     console.log('bottonm up');
-//     paralaxTop.scrollIntoView({behavior: "smooth"})
-//   }
-// });
-
-// paralaxTop.addEventListener('scroll', () => {
-//   console.log(scrollDirection);
-// });
-
 const gztCopyText = document.querySelector('.gzt__contract');
 
 gztCopyText.addEventListener('click', (event) => {
